9c12be271e58a74330c499a0ef090bdbe8eee325
HITGEA <61420673+HITGEA@users.noreply.github.com>
2023-01-24 10:54:20 +0200

    Update streamlit_app.py
    Added text and header fields

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled, Free-Range Egg')
