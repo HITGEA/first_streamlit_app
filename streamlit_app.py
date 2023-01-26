a5447d01daa89000f7104d910c9856c5ece56889
HITGEA <61420673+HITGEA@users.noreply.github.com>
2023-01-26 12:01:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,6 @@ streamlit.header('Which fruit would you like to add?')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Which fruit would you like to add:", list(my_data_rows))
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_data_rows.loc[fruits_selected]
 
 # If you want to know more about pandas.dataframe.loc[ ], you find more information here: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html 
